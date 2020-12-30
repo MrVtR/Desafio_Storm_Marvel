56acc4aa2ad23731f7b7e7b9a7f8db77250cae8a
MrVtR <30987772+MrVtR@users.noreply.github.com>
2020-12-30 18:39:12 -0300

    Componente de Filtro de Personagens finalizado

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,11 +2,70 @@ import "./Header.scss";
 import {Link} from "react-router-dom";
 import icon from "../../assets/icons/search-icon-red-md.png";
 import logo from "../../assets/images/marvelLogo.png"
+import getApi from "../../services/getApi";
+import {useState} from 'react'
 
 export default function Header(props) {
   //const { content, src, alt, className, onClick } = props;
+  const [filtro,setFiltro] = useState(false);
 
-  return (
+  async function VerInput(){
+    console.log("Tamanho:",document.getElementById("input").value.length);
+    if(document.getElementById("input").value.length> 0){
+      const gateway = 'http://gateway.marvel.com/v1/public/characters?nameStartsWith='+document.getElementById("input").value+'&ts=';
+      const responseFiltro = await getApi(gateway);
+      setFiltro(responseFiltro);
+      // console.log(responseFiltro)
+    }
+    else{
+      setFiltro(false);
+    }
+  }
+  if(filtro && document.getElementById("input").value.length > 0)
+  {
+    // console.log(filtro.data.results);
+    return (
+      <>
+      <div className="header">
+        <Link to="/">
+        <img src={logo} className="logo" alt=""></img>
+        </Link>
+        <div className="search">
+          <label htmlFor="checkbox">
+            <img src={icon} className="icon" alt=""></img>
+            <div className="filtro">
+              {filtro.data.results.map((storie,key) => 
+                <div className="personagemFiltro" key={key}>     
+                <Link to={{
+                  pathname: "/detail",
+                  comics: storie.comics,
+                  events: storie.events,
+                  series:storie.series,
+                  stories: storie.stories,
+                  thumbnail: storie.thumbnail,
+                  name: storie.name,
+                  description: storie.description
+                }}>
+                  <img src={storie.thumbnail.path+"/standard_medium."+storie.thumbnail.extension} alt=""></img>
+                  <h1 className="filtroText">{storie.name}</h1>
+                  </Link>            
+                </div>
+                )}
+            </div>
+          </label>
+          <div className="inputDiv">
+            <input type="checkbox" id="checkbox"></input>
+            <input className="input" type="text" id="input" onChange={VerInput}></input>  
+           
+          </div>
+        </div>
+      </div>
+      
+      </>
+  );
+  }
+  else{
+    return (
       <div className="header">
         <Link to="/">
           <img src={logo} className="logo" alt=""></img>
@@ -15,10 +74,14 @@ export default function Header(props) {
         <label htmlFor="checkbox">
           <img src={icon} className="icon" alt=""></img>
         </label>
+        <div>
         <input type="checkbox" id="checkbox"></input>
-        <input className="input" type="text"></input>
+        <input className="input" type="text" id="input" onChange={VerInput}></input>  
         </div>
-        
+        </div>
       </div>
   );
+  }
+  
 }
+
--- src/pages/CharacterDetail/CharacterDetail.js
+++ src/pages/CharacterDetail/CharacterDetail.js
@@ -3,109 +3,109 @@ import getApi from "../../services/getApi";
 import { Header,Loading } from '../../components/index'
 import { useState,useEffect } from 'react';
 
-function CharacterDetail(props) {
-  const {name,id,comics,events,series,stories,thumbnail,description} = props.location;
-  const [comicsData, setComicsData]=useState([]);
-  const [eventsData, setEventsData]=useState([]);
-  const [seriesData, setSeriesData]=useState([]);
-  const [storiesData, setStoriesData]=useState([]);
-  const [loading, setLoading]=useState(true);
+function CharacterDetail(props,location) {
+const {name,comics,events,series,stories,thumbnail,description} = props.location;
+const [comicsData, setComicsData]=useState([]);
+const [eventsData, setEventsData]=useState([]);
+const [seriesData, setSeriesData]=useState([]);
+const [storiesData, setStoriesData]=useState([]);
+const [loading, setLoading]=useState(true);
 
-  useEffect(() => {
-    (async () =>{
-      const comicsMap = await Promise.all(comics.items.map(async (comic,key) => 
-        await getApi(comic.resourceURI+"?ts=")
-      ));
-      
-      const eventsMap = await Promise.all(events.items.map(async (event,key) => 
-        await getApi(event.resourceURI+"?ts=")));
-
-      const seriesMap = await Promise.all(series.items.map(async (serie,key) => 
-        await getApi(serie.resourceURI+"?ts=")));
-
-      const storiesMap = await Promise.all(stories.items.map(async (storie,key) => 
-        await getApi(storie.resourceURI+"?ts=")));
-
-        setLoading(false);
-        console.log(storiesMap);
-        setComicsData(comicsMap);
-        setEventsData(eventsMap);
-        setSeriesData(seriesMap);
-        setStoriesData(storiesMap);
-    })()
-  }, [comics.items,events.items,series.items,stories.items])
-  if(loading)
-  {
-    console.log("Carregando");
-    return(
-      <Loading/>
-    )
-  }
+useEffect(() => {
+  (async () =>{
+    const comicsMap = await Promise.all(comics.items.map(async (comic,key) => 
+      await getApi(comic.resourceURI+"?ts=")
+    ));
     
-  else
-  {
-    console.log("Carregou");
-    return (
-      <>
-      <Header/>
-      <div className="Detail">
-        <div className="character">
-          <img src={thumbnail.path+"/landscape_incredible.jpg"} alt="" className="characterImage"/>
-          <div className="nameDescription">
-            <p className="cTitle">Name:</p>
-              <p className="cComponent">{name}</p>
-            <p className="cTitle">Description:</p>
-              <p className="cComponent">{description}</p>
-          </div>
-        </div>
-        <div className="title">Comics</div>
-        <div className="Images" width="400px">
+    const eventsMap = await Promise.all(events.items.map(async (event,key) => 
+      await getApi(event.resourceURI+"?ts=")));
 
-          {comicsData.map((comic, key) => (
-            [
-              <figure key={comic.data.results[0].id}>
-                <img src={comic.data.results[0].thumbnail.path+".jpg"} alt="" width="350px" height="auto" className="comicImage"/>
-                <figcaption><h1 className="comicTitle">{comic.data.results[0].title}</h1></figcaption>
-              </figure>
-            ]
-          ))}
-        </div>
-        <div className="title">Series</div>
-        <div className="Images">
-          {seriesData.map((serie, key) => ([
-            <figure key={serie.data.results[0].id}>
-              <img src={serie.data.results[0].thumbnail.path+".jpg"} alt="" width="350px" height="350px" className="comicImage"/>
-              <figcaption><h1 className="comicTitle">{serie.data.results[0].title}</h1></figcaption>
-            </figure>
-          ]))}
-        </div>
-        <div className="title">Stories</div>
-        <div className="Images">
-          {storiesData.map((storie, key) => (
-            storie && ( 
-            <figure key={storie.data.results[0].id}>
-              {storie.data.results[0].thumbnail?.path && <img src={storie.data.results[0].thumbnail.path+".jpg"} alt="" width="350px" height="350px" className="comicImage"/>}
-              <figcaption><h1 className="comicTitle">{storie.data.results[0].title}</h1></figcaption>
-            </figure>
-            )
-          ))}
-        </div>
+    const seriesMap = await Promise.all(series.items.map(async (serie,key) => 
+      await getApi(serie.resourceURI+"?ts=")));
 
-        <div className="title">Events</div>
-        <div className="Images">
-          {eventsData.map((event, key) => (
-            [
-              <figure key={event.data.results[0].id}>
-                <img src={event.data.results[0].thumbnail.path+".jpg"} alt="" width="350px" height="auto" className="comicImage"/>
-                <figcaption><h1 className="comicTitle">{event.data.results[0].title}</h1></figcaption>
-              </figure>
-            ]
-          ))}
+    const storiesMap = await Promise.all(stories.items.map(async (storie,key) => 
+      await getApi(storie.resourceURI+"?ts=")));
+
+      setLoading(false);
+      console.log(storiesMap);
+      setComicsData(comicsMap);
+      setEventsData(eventsMap);
+      setSeriesData(seriesMap);
+      setStoriesData(storiesMap);
+  })()
+}, [comics.items,events.items,series.items,stories.items])
+if(loading)
+{
+  console.log("Carregando");
+  return(
+    <Loading/>
+  )
+}
+  
+else
+{
+  console.log("Carregou");
+  return (
+    <>
+    <Header/>
+    <div className="Detail">
+      <div className="character">
+        <img src={thumbnail.path+".jpg"} alt="" className="characterImage"/>
+        <div className="nameDescription">
+          <p className="cTitle">Name:</p>
+            <p className="cComponent">{name}</p>
+          <p className="cTitle">Description:</p>
+            <p className="cComponent">{description}</p>
         </div>
       </div>
-      </>
-    );
-  }
+      <div className="title">Comics</div>
+      <div className="Images" width="400px">
+
+        {comicsData.map((comic, key) => (
+          [
+            <figure key={key}>
+              <img src={comic.data.results[0].thumbnail.path+".jpg"} alt="" width="350px" height="auto" className="comicImage"/>
+              <figcaption><h1 className="comicTitle">{comic.data.results[0].title}</h1></figcaption>
+            </figure>
+          ]
+        ))}
+      </div>
+      <div className="title">Series</div>
+      <div className="Images">
+        {seriesData.map((serie, key) => ([
+          <figure key={key}>
+            <img src={serie.data.results[0].thumbnail.path+".jpg"} alt="" width="350px" height="350px" className="comicImage"/>
+            <figcaption><h1 className="comicTitle">{serie.data.results[0].title}</h1></figcaption>
+          </figure>
+        ]))}
+      </div>
+      <div className="title">Stories</div>
+      <div className="Images">
+        {storiesData.map((storie, key) => (
+          storie && ( 
+          <figure key={key}>
+            {storie.data.results[0].thumbnail?.path && <img src={storie.data.results[0].thumbnail.path+".jpg"} alt="" width="350px" height="350px" className="comicImage"/>}
+            <figcaption><h1 className="comicTitle">{storie.data.results[0].title}</h1></figcaption>
+          </figure>
+          )
+        ))}
+      </div>
+
+      <div className="title">Events</div>
+      <div className="Images">
+        {eventsData.map((event, key) => (
+          [
+            <figure key={key}>
+              <img src={event.data.results[0].thumbnail.path+".jpg"} alt="" width="350px" height="auto" className="comicImage"/>
+              <figcaption><h1 className="comicTitle">{event.data.results[0].title}</h1></figcaption>
+            </figure>
+          ]
+        ))}
+      </div>
+    </div>
+    </>
+  );
+}
    
   }
 
