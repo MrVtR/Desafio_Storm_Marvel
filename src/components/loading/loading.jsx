da3bef38feef0a1ef155bcec7723843e583fc530
MrVtR <30987772+MrVtR@users.noreply.github.com>
2021-01-04 12:58:43 -0300

    Code Review do Loading feito e adicionado logo para o componente

--- src/components/loading/loading.jsx
+++ src/components/loading/loading.jsx
@@ -1,8 +1,10 @@
 import './loading.scss';
+import avengers from '../../assets/images/avengers.gif';
 export default function Loading() {
   return (
     <div className="loading">
       <h1>Carregando...</h1>
+      <img src={avengers} alt="" className="logoLoading" />
     </div>
   );
 }
